45f1bab5e194ce928b885b05b8e3789587fdb133
kevfree-max <kevfree@outlook.ie>
2026-03-02 15:10:16 +0000

    Create Program.cs with main entry point for the web service

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PracticionerSearchService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Practicioner Search Service!");
+        }
+    }
+}
